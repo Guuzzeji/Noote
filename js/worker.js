102e578d671401fda7e39a8748d191fc314f07d3
Guuzzeji <46883697+Guuzzeji@users.noreply.github.com>
2021-08-24 14:26:46 -0700

    Delete worker.js

--- js/worker.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('./pwa.js')
-        .then(function () {
-            console.log('Service Worker Registered');
-        });
-}
